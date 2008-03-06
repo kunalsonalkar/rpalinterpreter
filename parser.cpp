72a2ff5ec0ad51649ab67d9f11a243bb0656e7ce
balaji.krishnan <balaji.krishnan@gmail.com>
2008-03-06 07:18:53 +0000

    Initial Revision

---
+++ parser.cpp
@@ -0,0 +1,154 @@
+#include "parser.h"
+
+//constructor
+Parser::Parser()
+{
+
+}
+
+Parser::Parser(string sFileName)
+{
+
+}
+
+//destructor
+Parser::~Parser()
+{
+}
+
+//Methods
+
+//#Expressions##############################
+bool Parser::proc_E()
+{
+
+    return true;
+}
+
+bool Parser::proc_Ew()
+{
+
+    return true;
+}
+
+//#Tuple Expressions##############################
+bool Parser::proc_T()
+{
+
+    return true;
+}
+
+bool Parser::proc_Ta()
+{
+
+    return true;
+}
+
+bool Parser::proc_Tc()
+{
+
+    return true;
+}
+
+//#Boolean Expressions##############################
+bool Parser::proc_B()
+{
+
+    return true;
+}
+
+bool Parser::proc_Bt()
+{
+
+ return true;
+}
+
+bool Parser::proc_Bs()
+{
+
+    return true;
+}
+
+bool Parser::proc_Bp()
+{
+
+    return true;
+}
+
+//#Arithmetic Expressions##############################
+bool Parser::proc_A()
+{
+
+    return true;
+}
+
+bool Parser::proc_At()
+{
+
+    return true;
+}
+
+bool Parser::proc_Af()
+{
+
+    return true;
+}
+
+bool Parser::proc_Ap()
+{
+
+
+    return true;
+}
+
+//#Rators And Rands##############################
+bool Parser::proc_R()
+{
+
+    return true;
+}
+
+bool Parser::proc_Rn()
+{
+
+    return true;
+}
+
+//#Definitions##############################
+bool Parser::proc_D()
+{
+
+    return true;
+}
+
+bool Parser::proc_Da()
+{
+
+    return true;
+}
+
+bool Parser::proc_Dr()
+{
+
+    return true;
+}
+
+bool Parser::proc_Db()
+{
+
+    return true;
+}
+
+//#Variables##############################
+bool Parser::proc_Vb()
+{
+
+    return true;
+}
+
+bool Parser::proc_Vl()
+{
+
+    return true;
+}
+
---
+++ scanner.h
@@ -0,0 +1,28 @@
+#ifndef SCANNER_H_INCLUDED
+#define SCANNER_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+class scanner
+{
+
+    private:
+        char* fileptr;
+        ifstream filestream;
+        int index, filesize;
+        bool isoperator(int c);
+        bool ispuncuation(int c);
+        bool isEOF();
+
+    public:
+        scanner(char* name);
+        ~scanner();
+        string getnexttoken();
+
+};
+
+#endif // SCANNER_H_INCLUDED
