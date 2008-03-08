6420eeab205e0614b05d351dc943e986f254d2c5
ramachandren <ramachandren@gmail.com>
2008-03-08 17:34:36 +0000

    Initial Revision

---
+++ lookaheadbuffer.h
@@ -0,0 +1,30 @@
+#ifndef LOOKAHEADBUFFER_H_INCLUDED
+#define LOOKAHEADBUFFER_H_INCLUDED
+
+#include "scanner.h"
+
+class LookAheadBuffer
+{
+    Token *currToken;
+    Token *lookAhead1;
+    Token *lookAhead2;
+
+    Scanner* oScanner;
+
+public:
+
+    LookAheadBuffer();
+    LookAheadBuffer(string sFileName);
+    ~LookAheadBuffer();
+
+    Token*   getCurrentToken();
+    Token*   getLookAhead1();
+    Token*   getLookAhead2();
+
+    void moveForward();
+
+};
+
+
+
+#endif // LOOKAHEADBUFFER_H_INCLUDED
